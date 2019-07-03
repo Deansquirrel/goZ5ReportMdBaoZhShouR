292081cecde1f23fff21c0c0ac75c40ca44e6e53
yuanxs <yuanxs@95szzl.cn>
2019-07-03 11:55:14 +0800

    201907031155

--- global/Global.go
+++ global/Global.go
@@ -6,9 +6,9 @@ import (
 )
 
 const (
-	//PreVersion = "0.0.0 Build20190101"
+	//PreVersion = "1.0.0 Build20190703"
 	//TestVersion = "0.0.0 Build20190101"
-	Version = "1.0.0 Build20190703"
+	Version = "0.0.0 Build20190101"
 
 	SecretKey        = "Z5ReportMdBaoZhShouR"
 	IsForbiddenTilte = "已禁用"
